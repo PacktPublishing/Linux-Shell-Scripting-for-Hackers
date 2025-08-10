2277cb6347fd287b385d8a9f92f6bba29ac17fa9
donniet4 <55449545+donniet4@users.noreply.github.com>
2025-08-10 14:26:55 -0400

    Add files via upload

--- chapter-5/nikto_scan-1.sh
+++ chapter-5/nikto_scan-1.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+./nikto.pl -h 192.168.56.102 -output metasploitable-home_page.html
+./nikto.pl -h http://192.168.56.102/dvwa -output metasploitable-dvwa.html
+./nikto.pl -h http://192.168.56.102/dvwa/vulnerabilities/sqli/ -output metasploitable-sqli.html
+./nikto.pl -h http://192.168.56.102/mutillidae -output metasploitable-mutillidae.html
+./nikto.pl -h http://192.168.56.102/dav -output metasploitable-webdav.html
+./nikto.pl -h http://192.168.56.102/twiki -output metasploitable-twiki.html
+./nikto.pl -h 192.168.56.103 -output Breakout-80.html
+./nikto.pl -h 192.168.56.103 -ssl -port 10000 -output Breakout-10000.html
+./nikto.pl -h 192.168.56.103 -ssl -port 20000 -output Breakout-20000.html
 
