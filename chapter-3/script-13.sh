cdad6d3111676cf0a040c6525976bc705612f64d
Nithik Cheruvakodan <77096057+nithikpackt@users.noreply.github.com>
2026-02-09 12:02:35 +0530

    Update script-13.sh

--- chapter-3/script-13.sh
+++ chapter-3/script-13.sh
@@ -4,5 +4,6 @@ if [ "$1" == "-v" ]; then
     echo "Running scan in verbose mode..."
     # Perform the scan in verbose mode
 else
+    echo "Running scan in normal mode..."
     # Perform the scan in normal mode
 fi
